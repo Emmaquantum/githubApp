18ca96125a5ce509077d8d36f8240be908f2289b
julianssvr <julianssvr@gmail.com>
2026-06-28 00:26:03 -0500

    Upload README

--- config/github.js
+++ config/github.js
@@ -6,7 +6,7 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN 
 });
 
-console.log('🔑 Token cargado:', process.env.GITHUB_TOKEN ? 'SÍ' : 'NO');
+console.log('Token cargado:', process.env.GITHUB_TOKEN ? 'SÍ' : 'NO');
 
 // Export the octokit instance for use in other modules
 module.exports = octokit;
